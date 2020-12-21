318600ad7abfaea459376c6d6673038141af39d5
cozyBoi <kjjh1126@gmail.com>
2020-12-21 16:15:36 +0900

    can exc

--- mvPortedSrc.sh
+++ mvPortedSrc.sh
@@ -3,29 +3,28 @@ PortedSrcPath="/home/jlee/Desktop/SSD/gpukv_src/kernel-4.9"
 SrcName=""
 
 #syscall_64.tbl
-SrcName="/tool/perf/arch/x86/syscall_64.tbl"
-mv $(OrgSrcPath)$(SrcName) $(OrgSrcPath)$(SrcName)".tmp"
-cp $(PortedSrcPath)$(SrcName) $(OrgSrcPath)$(SrcName)
+SrcName="/tools/perf/arch/x86/entry/syscalls/syscall_64.tbl"
+mv "$OrgSrcPath""$SrcName" "$OrgSrcPath""$SrcName"".tmp"
+cp "$PortedSrcPath""$SrcName" "$OrgSrcPath""$SrcName"
 
 #syscall.h
 SrcName="/include/linux/syscall.h"
-mv $(OrgSrcPath)$(SrcName) $(OrgSrcPath)$(SrcName)".tmp"
-cp $(PortedSrcPath)$(SrcName) $(OrgSrcPath)$(SrcName)
+mv "$OrgSrcPath""$SrcName" "$OrgSrcPath""$SrcName"".tmp"
+cp "$PortedSrcPath""$SrcName" "$OrgSrcPath""$SrcName"
 
 #keyvalue.c kernel/Makefile
 SrcName="/kernel/Makefile"
-mv $(OrgSrcPath)$(SrcName) $(OrgSrcPath)$(SrcName)".tmp"
-cp $(PortedSrcPath)$(SrcName) $(OrgSrcPath)$(SrcName)
-
-cp $(PortedSrcPath)"/kernel/keyvalue.c"  $(SrcName)"/kernel/keyvalue.c"
+mv "$OrgSrcPath""$SrcName" "$OrgSrcPath""$SrcName"".tmp"
+cp "$PortedSrcPath""$SrcName" "$OrgSrcPath""$SrcName"
 
 #pci.c
 SrcName="/driver/nvme/host/pci.c"
-mv $(OrgSrcPath)$(SrcName) $(OrgSrcPath)$(SrcName)".tmp"
-cp $(PortedSrcPath)$(SrcName) $(OrgSrcPath)$(SrcName)
+mv "$OrgSrcPath""$SrcName" "$OrgSrcPath""$SrcName"".tmp"
+cp "$PortedSrcPath""$SrcName" "$OrgSrcPath""$SrcName"
 
 #nvme.h
 SrcName="/include/linux/nvme.h"
-mv $(OrgSrcPath)$(SrcName) $(OrgSrcPath)$(SrcName)".tmp"
-cp $(PortedSrcPath)$(SrcName) $(OrgSrcPath)$(SrcName)
+mv "$OrgSrcPath""$SrcName" "$OrgSrcPath""$SrcName"".tmp"
+cp "$PortedSrcPath""$SrcName" "$OrgSrcPath""$SrcName"
+
 
